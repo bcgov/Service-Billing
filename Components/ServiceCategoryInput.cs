fea2cd1d249fe584d8b9a6aad50ffe56a2c86a68
Carmichael <Alexander.Carmichael@gov.bc.ca>
2023-06-28 11:08:05 -0700

    Service Category input now its own ViewComponent

---
+++ Components/ServiceCategoryInput.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service_Billing.Models;
+
+namespace Service_Billing.Components
+{
+    public class ServiceCategoryInput : ViewComponent
+    {
+        public IViewComponentResult Invoke(IEnumerable<ServiceCategory> categories, short id)
+        {
+            ViewData["id"] = id;
+            return View(categories);
+        }
+    }
+}
