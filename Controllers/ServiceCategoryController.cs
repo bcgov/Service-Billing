b7fcbb1bf1ab607eb62ee584b15085cf652dd21f
Carmichael <Alexander.Carmichael@gov.bc.ca>
2023-08-08 15:11:02 -0700

    Service Categories index

---
+++ Controllers/ServiceCategoryController.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Service_Billing.Models;
+using Service_Billing.ViewModels;
+
+namespace Service_Billing.Controllers
+{
+    public class ServiceCategoryController: Controller
+    {
+        private readonly IServiceCategoryRepository _categoryRepository;
+
+        public ServiceCategoryController(IServiceCategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ServiceCategory> categories = _categoryRepository.GetAll();
+            return View(categories);
+        }
+    }
+}
