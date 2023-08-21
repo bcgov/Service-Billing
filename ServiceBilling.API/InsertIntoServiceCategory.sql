f35b719198b4ee0891a9005fe558a231a7c44af3
Carmichael <Alexander.Carmichael@gov.bc.ca>
2023-08-21 10:50:21 -0700

    Create InsertIntoServiceCategory.sql

---
+++ ServiceBilling.API/InsertIntoServiceCategory.sql
@@ -0,0 +1,5 @@
+﻿
+INSERT INTO [(localdb)\MSSQLLocalDB.aspnet-WebApplication2-0441b489-9723-4770-814a-c630fcefdc34].dbo.ServiceCategories
+(ServiceCategoryId, GdxBusinessArea, [Name], Costs, [Description], UOM)
+Select ServiceId, GDXBusArea, [Name], Costs, [Description], UOM 
+From Billing.dbo.ServiceCategories;
