35bd6d927c4b0725c9c4101dd0a46901188c7271
Carmichael <Alexander.Carmichael@gov.bc.ca>
2023-08-29 13:34:24 -0700

    partial view added to API project

---
+++ ServiceBilling.API/ServiceBilling.BillingManagement.UI/ViewModels/ContactSelectViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ServiceBilling.BillingManagement.UI.ViewModels
+{
+    public class ContactSelectViewModel
+    {
+        public string? elementId { get; set; }
+        public IEnumerable<SelectListItem> Contacts { get; set; }
+
+        public ContactSelectViewModel(string? id, IEnumerable<SelectListItem> list)
+        {
+            elementId = id;
+            Contacts = list;
+        }
+    }
+}
---
+++ ServiceBilling.API/exampleInsertFromOneServerToLocal.sql
@@ -0,0 +1,6 @@
+﻿insert into ServiceCategories
+(GdxBusinessArea, [Name], Costs, [Description], UOM)
+Select GDXBusArea, [Name], Costs, [Description], UOM 
+from [NH500919].[Billing].[dbo].[ServiceCategories];
+
+/* Had to make modifications to receiving table: id gets default value of NEWID(), Dates get default value of GETDATE() */
