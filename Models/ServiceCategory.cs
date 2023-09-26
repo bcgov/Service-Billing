081dd23aa68fd9c92a0523e1c29577954d698457
Carmichael <Alexander.Carmichael@gov.bc.ca>
2023-09-26 16:37:12 -0700

    UI improvements for all index views

--- Models/ServiceCategory.cs
+++ Models/ServiceCategory.cs
@@ -7,7 +7,8 @@ namespace Service_Billing.Models
     {
         [Key]
         public int ServiceId { get; set; }
-
+        
+        [Display(Name = "GDX Business Area")]
         public string? GDXBusArea { get; set; }
 
         public string? Name { get; set; }
@@ -16,10 +17,12 @@ namespace Service_Billing.Models
 
         public string? Description { get; set; }
 
+        [Display(Name = "Is Active?")]
         public bool IsActive { get; set; }
 
         public string? UOM { get; set; }
 
+        [Display(Name = "Service Owner")]
         public string? ServiceOwner { get; set; }
     }
 }
