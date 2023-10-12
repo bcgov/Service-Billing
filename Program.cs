e23cc03e6d9906ab09848ec73643049e7e556d93
André Lashley <andre.lashley@gmail.com>
2023-10-12 16:00:12 -0700

    Add automatic migrations

--- Program.cs
+++ Program.cs
@@ -119,6 +119,13 @@ if (!app.Environment.IsDevelopment())
 
 app.UseSession();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ServiceBillingContext>();
+    await context.Database.MigrateAsync();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseCookiePolicy();
