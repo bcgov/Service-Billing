cd89b0c37b315cd1e5b8242e7b87938725455bac
André Lashley <andre.lashley@gmail.com>
2023-11-10 15:22:03 -0800

    Enable automatic migrations

--- Program.cs
+++ Program.cs
@@ -187,7 +187,7 @@ using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     var context = services.GetRequiredService<ServiceBillingContext>();
-    //await context.Database.MigrateAsync();
+    await context.Database.MigrateAsync();
     //DbInitializer.SeedMinistries(context);
     //DbInitializer.SeedServices(context);
     //DbInitializer.SeedTeams(context);
