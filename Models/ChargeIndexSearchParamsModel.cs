740332cd3ab47a07546982c7962ff72077d1da32
Carmichael <Alexander.Carmichael@gov.bc.ca>
2023-12-05 17:24:42 -0800

    refactored charge searching in billscontroller

---
+++ Models/ChargeIndexSearchParamsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Service_Billing.Models
+{
+    public class ChargeIndexSearchParamsModel
+    {
+        public string? QuarterFilter { get; set; }
+        public string? MinistryFilter { get; set; }
+        public string? TitleFilter { get; set; }
+        public int? CategoryFilter { get; set; }
+        public string? AuthorityFilter { get; set; }
+        public int? ClientNumber {  get; set; }
+        public string? Keyword {  get; set; }
+        public bool? Inactive { get; set; }
+    }
+}
