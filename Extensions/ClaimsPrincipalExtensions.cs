e03284a144b5c876948eb37cbc390ffa732b5d23
André Lashley <andre.lashley@gmail.com>
2023-12-07 15:38:31 -0800

    Finish initial cut of ministry client access

--- Extensions/ClaimsPrincipalExtensions.cs
+++ Extensions/ClaimsPrincipalExtensions.cs
@@ -6,7 +6,7 @@ namespace Service_Billing.Extensions
     {
         public static bool IsMinistryClient(this ClaimsPrincipal user)
         {
-            var rvl = user?.Claims?.Any(c => c.Type == "groups" && c.Value == "") ?? false;
+            var rvl = user?.Claims?.Any(c => c.Type == "groups" && c.Value == "9e2cea8d-0dc8-4d4f-9bc9-d1930932d335") ?? false;
             return rvl;
         }
     }
