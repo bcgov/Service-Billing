1b3f46f06a320bcd1800cb8f0576ed63a14f2052
André Lashley <andre.lashley@gmail.com>
2024-02-09 11:25:41 -0800

    Disable sending email notifications for the time being

---
+++ Controllers/API/PeopleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service_Billing.Models.Repositories;
+
+namespace Service_Billing.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly IBillRepository _billRepository;
+
+        public PeopleController(ILogger<PeopleController> logger,
+            IBillRepository billRepository)
+        {
+            _billRepository = billRepository;
+        }
+
+        public IActionResult Get()
+        {
+            //var people = _billRepository
+            //    .AllBills
+            //        .Where(b => b.ClientAccount is not null 
+            //                    && b.ClientAccount.Team is not null 
+            //                    && b.ClientAccount.Team.PrimaryContact is not null)
+            //            .Select(b => b.ClientAccount.Team.PrimaryContact);
+
+
+            //return Ok(people);
+            return Ok();
+        }
+    }
+}
