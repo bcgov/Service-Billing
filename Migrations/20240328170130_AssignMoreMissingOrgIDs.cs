72b86b9d2f0a8dfb284c5cf13f59bed349501cad
Carmichael <Alexander.Carmichael@gov.bc.ca>
2024-03-28 15:37:41 -0700

    Update 20240328170130_AssignMoreMissingOrgIDs.cs

--- Migrations/20240328170130_AssignMoreMissingOrgIDs.cs
+++ Migrations/20240328170130_AssignMoreMissingOrgIDs.cs
@@ -11,9 +11,9 @@ namespace Service_Billing.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             object[] clientsMissingOrgs = { 818, 2009, 835, 2053, 744, 705, 797, 859, 2111, 815, 854, 845, 675, 633, 2048, 2049, 812, 769,
-            747, 862, 2110, 826, 837, 849, 621, 752, 681, 784, 820, 848, 750, 806, 802, 2079 };
+            747, 862, 2110, 826, 837, 849, 621, 752, 681, 784, 820, 848, 750, 806, 802, 2079, 858, 680, 810 };
             object[] orgIDsToAssign = { 4, 4, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 7, 8, 8, 8, 10, 11,
-            12, 13, 46, 16, 17, 17, 40, 40, 20, 20, 22, 22, 22, 44, 24, 24  };
+            12, 13, 46, 16, 17, 17, 40, 40, 20, 20, 22, 22, 22, 44, 24, 24, 20, 13, 13  };
             migrationBuilder.UpdateData(
                 table: "ClientAccounts",
                 keyColumn: "Id",
