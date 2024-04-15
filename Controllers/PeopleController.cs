c09061a75fbeabccc146e9ca09c0a6f5926d5b4b
André Lashley <andre.lashley@gmail.com>
2024-04-15 11:33:55 -0700

    Add People model

---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service_Billing.Models.Repositories;
+
+namespace Service_Billing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly IBillRepository _billRepository;
+
+        public PeopleController(IBillRepository billRepository)
+        {
+            _billRepository = billRepository;
+        }
+
+        public IActionResult Get()
+        {
+            var people = _billRepository.AllBills.Select(b => b.ClientAccount.PrimaryContact).ToList();
+
+            return Ok(people); 
+        }
+    }
+}
