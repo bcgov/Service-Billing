841c5b041a74602f88488f32f126373bf3ded2a3
Carmichael <Alexander.Carmichael@gov.bc.ca>
2024-08-21 13:56:23 -0700

    bil-92 validation on client create

--- Models/ClientAccount.cs
+++ Models/ClientAccount.cs
@@ -44,8 +44,6 @@ o	Note: not all naming convention components are required. The naming convention
         [Display(Name = "Project")]
         public string? Project { get; set; }
 
-
-        [Required(ErrorMessage = "Please include this contact")]
         [Display(Name = "Expense Authority")]
         public string? ExpenseAuthorityName { get; set; }
 
