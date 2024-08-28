a7c67a94390e3ec012674bd2a2e29fe08f311d8b
Carmichael <Alexander.Carmichael@gov.bc.ca>
2024-08-28 13:50:30 -0700

    updated for readability

--- Migrations/20240711224046_MapBillsToFiscalPeriods.cs
+++ Migrations/20240711224046_MapBillsToFiscalPeriods.cs
@@ -14,9 +14,22 @@ namespace Service_Billing.Migrations
          */
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("insert into FiscalPeriods ([Period])\r\n    select distinct Bills.FiscalPeriod\r\n    from Bills\r\n    where not exists (select 1 from FiscalPeriods where FiscalPeriods.[Period] = Bills.FiscalPeriod);\r\nGO\r\n\r\nupdate Bills\r\nset CurrentFiscalPeriodId = FiscalPeriods.Id\r\nfrom Bills\r\nINNER JOIN\r\n    FiscalPeriods\r\nON \r\n    Bills.FiscalPeriod = FiscalPeriods.[Period];\r\n\r\n\tGO");
+            migrationBuilder.Sql($"insert into FiscalPeriods " +
+                $"([Period]) " +    
+                $"select distinct Bills.FiscalPeriod " +
+                $"from Bills " + 
+                $"where not exists (select 1 from FiscalPeriods where FiscalPeriods.[Period] = Bills.FiscalPeriod); " + 
+                $"GO " +
+                $"update Bills " + 
+                $"set CurrentFiscalPeriodId = FiscalPeriods.Id " + 
+                $"from Bills " + 
+                $"INNER JOIN " +   
+                $"FiscalPeriods " + 
+                $"ON " +    
+                $"Bills.FiscalPeriod = FiscalPeriods.[Period]; " +
+                $"GO"
+                );
         }
-
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
