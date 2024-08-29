982f967776b8e05b0543643bd4c42842f67578c3
Carmichael <Alexander.Carmichael@gov.bc.ca>
2024-08-29 15:41:52 -0700

    Update 20240829204100_InsertQ1-24-25FiscalHistoryRecords.cs

--- Migrations/20240829204100_InsertQ1-24-25FiscalHistoryRecords.cs
+++ Migrations/20240829204100_InsertQ1-24-25FiscalHistoryRecords.cs
@@ -11,8 +11,8 @@ namespace Service_Billing.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(
-                "INSERT INTO FiscalHistory (BillId, PeriodId, UnitPriceAtFiscal)\r\nSELECT \r\n  B.Id,\r\n  FP2.Id,\r\n  SC.Costs\r\nFROM \r\n  FiscalPeriod FP\r\n  INNER JOIN FiscalPeriods FP2 ON FP.[Period] = FP2.[Period]\r\n  INNER JOIN Bills B ON FP.ChargeId = B.Id\r\n  INNER JOIN ServiceCategories SC ON B.ServiceCategoryId = SC.ServiceId\r\nWHERE \r\n  FP.[Period] = 'Fiscal 24/25 Quarter 1'\r\n  AND NOT EXISTS (\r\n    SELECT 1\r\n    FROM FiscalHistory FH\r\n    WHERE FH.BillId = B.Id AND FH.PeriodId = FP2.Id\r\n  );\r\n  GO;\r\n\r\n  update FiscalHistory\r\n  set QuantityAtFiscal = 3 \r\n  where\r\n  PeriodId = 3\r\n  Go;\r\n  update FiscalHistory\r\n  set QuantityAtFiscal = 0\r\n  where\r\n  PeriodId = 3 and BillId = 11832 or BillId = 11833\r\n  Go;"
-             ); 
+                "INSERT INTO FiscalHistory (BillId, PeriodId, UnitPriceAtFiscal)\r\nSELECT \r\n  B.Id,\r\n  FP2.Id,\r\n  SC.Costs\r\nFROM \r\n  FiscalPeriod FP\r\n  INNER JOIN FiscalPeriods FP2 ON FP.[Period] = FP2.[Period]\r\n  INNER JOIN Bills B ON FP.ChargeId = B.Id\r\n  INNER JOIN ServiceCategories SC ON B.ServiceCategoryId = SC.ServiceId\r\nWHERE \r\n  FP.[Period] = 'Fiscal 24/25 Quarter 1'\r\n  AND NOT EXISTS (\r\n    SELECT 1\r\n    FROM FiscalHistory FH\r\n    WHERE FH.BillId = B.Id AND FH.PeriodId = FP2.Id\r\n  );\r\n\r\n\r\n  update FiscalHistory\r\n  set QuantityAtFiscal = 3 \r\n  where\r\n  PeriodId = 3;\r\n\r\n  update FiscalHistory\r\n  set QuantityAtFiscal = 0\r\n  where\r\n  PeriodId = 3 and BillId = 11832 or BillId = 11833;"
+             );  
         }
 
         /// <inheritdoc />
