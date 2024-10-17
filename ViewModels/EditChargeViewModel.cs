e2b7ca59ef3a20eea4c03067e50ddc97de26d018
Carmichael <Alexander.Carmichael@gov.bc.ca>
2024-10-17 13:17:05 -0700

    bil-173

---
+++ ViewModels/EditChargeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Service_Billing.Models;
+
+namespace Service_Billing.ViewModels
+{
+    public class EditChargeViewModel
+    {
+        public Bill Bill { get; set; }
+        public FiscalHistory? FiscalHistory { get; set; }
+
+        public EditChargeViewModel (Bill bill, FiscalHistory? fiscalHistory = null)
+        {
+            Bill = bill;
+            FiscalHistory = fiscalHistory;
+        }
+    }
+}
