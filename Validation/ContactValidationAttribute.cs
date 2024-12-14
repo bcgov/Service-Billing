88239ca0d5c56ab460811514c7f9059c06731dea
Carmichael <Alexander.Carmichael@gov.bc.ca>
2024-12-13 17:31:18 -0800

    bil-214, 215

--- Validation/ContactValidationAttribute.cs
+++ Validation/ContactValidationAttribute.cs
@@ -11,10 +11,10 @@
         {
             var contacts = value as ICollection<Contact>;
 
-            if (contacts == null || !contacts.Any())
-            {
-                return new ValidationResult("At least one contact is required.");
-            }
+            //if (contacts == null || !contacts.Any())
+            //{
+            //    return new ValidationResult("At least one contact is required.");
+            //}
 
             var primaryContacts = contacts.Where(c => c.ContactType == "primary").ToList();
             var approverContacts = contacts.Where(c => c.ContactType == "approver").ToList();
@@ -26,12 +26,16 @@
             }
             if(primaryContacts.Count > 2)
             {
-                return new ValidationResult("There may be no more than two primart contacts");
+                return new ValidationResult("There may be no more than two primary contacts");
             }
 
-            if (!approverContacts.Any() && !financialContacts.Any())
+            if (!approverContacts.Any())
             {
-                return new ValidationResult("At least one contact of type 'approver' or 'financial' is required.");
+                return new ValidationResult("Please provide at least one approver contact");
+            }
+            if(!financialContacts.Any())
+            {
+                return new ValidationResult("Please provide at least one financial contact");
             }
 
             return ValidationResult.Success;
